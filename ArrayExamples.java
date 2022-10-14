67b5ab17c6ca4671a09b6784107ea96fbba49993
pviwatkurkul <pviwatkurkul@ucsd.edu>
2022-10-14 15:18:23 -0700

    add test

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,6 +3,7 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
+  
   static void reverseInPlace(int[] arr) {
     int temp = 0;
     for(int i = 0; i < arr.length/2; i += 1) {
@@ -11,6 +12,14 @@ public class ArrayExamples {
       arr[arr.length - i - 1] = temp;
     }
   }
+/* 
+
+  static void reverseInPlace(int[] arr) {
+    for(int i = 0; i < arr.length; i += 1) {
+      arr[i] = arr[arr.length - i - 1];
+    }
+  }
+ */
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
